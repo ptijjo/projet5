38be4f4994e3643563e7227aa2a38efd4707e01e
ptijjo <bangouraibrahima57@gmail.com>
2022-08-30 22:53:21 +0200

    Modifications des quantites

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,44 +1,45 @@
 
 let commande =JSON.parse(localStorage.getItem("commande"));
 
+
 function saveArticle(produit){
     return localStorage.setItem("commande",JSON.stringify(produit));  
    }
+
 
 class Quantite{
     constructor(id,couleur,nombre){
         this.id=id;
         this.couleur=couleur;
-        this.quantite=nombre;        
+        this.quantite=nombre;       
     }
 }
 
-//console.log("voici ma commande",commande);
 if (commande==null){
     document.getElementById("cart__items").innerHTML= `<p> Votre panier est vide</p>`;
 }
 else{
-    commande.forEach(element => {
+    for (let i=0; i<commande.length; i++) {
 
-        fetch(`http://localhost:3000/api/products/${element.id}`)
+        fetch(`http://localhost:3000/api/products/${commande[i].id}`)
             .then (data => data.json())
             .then (panier => {                 
                 
-                
-                document.getElementById("cart__items").innerHTML+=  `<article class="cart__item" data-id="${element.id}" data-color="${element.couleur}">
+                console.log(panier);
+                document.getElementById("cart__items").innerHTML+=  `<article class="cart__item" data-id="${commande[i].id}" data-color="${commande[i].couleur}">
                                         <div class="cart__item__img">
                                             <img src="${panier.imageUrl}" alt="${panier.altTxt}">
                                         </div>
                                         <div class="cart__item__content">
                                             <div class="cart__item__content__description">
                                                 <h2>${panier.name}</h2>
-                                                <p>${element.couleur}</p>
+                                                <p>${commande[i].couleur}</p>
                                                 <p> ${panier.price}€</p>
                                             </div>
                                             <div class="cart__item__content__settings">
                                                 <div class="cart__item__content__settings__quantity">
                                                     <p>Qté :</p>
-                                                    <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value=${element.quantite}>
+                                                    <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value=${commande[i].quantite}>
                                                 </div>
                                                 <div class="cart__item__content__settings__delete">
                                                     <p class="deleteItem">Supprimer</p>
@@ -52,20 +53,19 @@ else{
                                 // Modification de quantité    
                                     let prixTotalArticle;
                                     let qte = document.getElementsByClassName("itemQuantity");
-                                    console.log("Nous avons ",qte.length," articles dont on peut modifier la qte");
+                                    
                                 for(let i=0; i<qte.length;i++){
+                                   
                                     qte[i].addEventListener("change",function(){                                   
                                                                                 
-                                        console.log(this.value);
-                                        let nvelleCommande = new Quantite(element.id,element.couleur,this.value);
-                                    
+                                        let nvelleCommande = new Quantite(commande[i].id,commande[i].couleur,this.value);                                        
                                         commande.splice(i,1,nvelleCommande);
-                                        console.log(commande);
-                                        saveArticle(commande);
-                                                                                                                 
-                                            
+                                        console.log(commande);     
+                                        localStorage.clear;
+                                        saveArticle(commande); 
+                                        console.log()                                                              
                                     })
-                                }
+                                }                         
                                         
              })
                 
@@ -79,7 +79,7 @@ else{
                     document.getElementById("totalQuantity").innerText=0;
                     
                 })*/
-    })
+    }
     
 
     

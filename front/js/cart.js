c07dcb828bb2d9bf70434a922e26a55e6378ab3d
ptijjo <bangouraibrahima57@gmail.com>
2022-08-31 11:06:16 +0200

    suppression d'article

--- front/js/cart.js
+++ front/js/cart.js
@@ -6,6 +6,10 @@ function saveArticle(produit){
     return localStorage.setItem("commande",JSON.stringify(produit));  
    }
 
+   function deleteArticle(article){
+    localStorage.removeItem(article);
+   }
+
 
 class Quantite{
     constructor(id,couleur,nombre){
@@ -18,6 +22,7 @@ class Quantite{
 if (commande==null){
     document.getElementById("cart__items").innerHTML= `<p> Votre panier est vide</p>`;
 }
+
 else{
     for (let i=0; i<commande.length; i++) {
 
@@ -25,7 +30,7 @@ else{
             .then (data => data.json())
             .then (panier => {                 
                 
-                console.log(panier);
+                //console.log(panier);
                 document.getElementById("cart__items").innerHTML+=  `<article class="cart__item" data-id="${commande[i].id}" data-color="${commande[i].couleur}">
                                         <div class="cart__item__img">
                                             <img src="${panier.imageUrl}" alt="${panier.altTxt}">
@@ -50,30 +55,65 @@ else{
 
                                     
 
-                                // Modification de quantité    
-                                    let prixTotalArticle;
-                                    let qte = document.getElementsByClassName("itemQuantity");
-                                    
-                                for(let i=0; i<qte.length;i++){
-                                   
-                                    qte[i].addEventListener("change",function(){                                   
-                                                                                
-                                        let nvelleCommande = new Quantite(commande[i].id,commande[i].couleur,this.value);                                        
-                                        commande.splice(i,1,nvelleCommande);
-                                        console.log(commande);     
-                                        localStorage.clear;
-                                        saveArticle(commande); 
-                                        console.log()                                                              
-                                    })
-                                }                         
+                // Modification de quantité    
+                let prixTotalArticle;
+                let qte = document.getElementsByClassName("itemQuantity");
+                    
+                for(let i=0; i<qte.length;i++){
+                    
+                    qte[i].addEventListener("change",function(){                                   
+                                                                
+                        let nvelleCommande = new Quantite(commande[i].id,commande[i].couleur,this.value);                                        
+                        commande.splice(i,1,nvelleCommande);
+                        console.log(commande);     
+                        localStorage.clear;
+                        saveArticle(commande);   
+                        fetch(`http://localhost:3000/api/products/${commande[i].id}`)
+                        .then (data => data.json())
+                        .then (prixQte => {     
+                            prixTotalArticle= prixQte.price*JSON.parse(nvelleCommande.quantite);
+                            console.log(prixTotalArticle);                          
+                            
+                        })
+                    })
+                }   
+                // Suppression d'element
+
+                let supprimer = document.getElementsByClassName("deleteItem")
+                //console.log(supprimer.length);
+                for(let i=0; i<supprimer.length;i++){
+                    supprimer[i].addEventListener("click", () =>{
+                        console.log("Article supprimé est : ",commande[i]);
+                        console.log(commande);
+                        commande.splice(i,1);
+                        console.log(commande);
+                        saveArticle(commande);
+                        location.reload();
+                        console.log(commande.length);
+                         if(commande.length=0){
+                            localStorage.clear();
+                        }
+                        
+
+
+                    })
+                    
+                }
+                
+                
                                         
-             })
+            })
                 
 
-                /*document.querySelector(".deleteItem").addEventListener("click",()=>{
+                
+                
+                /*supprimer.addEventListener("click",()=>{
                     console.log("Je supprime l'article", element);
                     localStorage.clear();
-                    resume.innerHTML=`<p> ${panier.name} est supprimé du panier </p>`;    
+                
+                }*/
+
+                    /*resume.innerHTML=`<p> ${panier.name} est supprimé du panier </p>`;    
                 
                     document.getElementById("totalPrice").innerText=0;
                     document.getElementById("totalQuantity").innerText=0;
